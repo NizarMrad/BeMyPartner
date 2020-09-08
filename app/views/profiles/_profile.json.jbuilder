529cb9e02fa398181d96f5de0cc8eba63175fd37
Halima-C <h.cherik@gmail.com>
2020-09-08 18:24:26 +0200

    update profile

--- app/views/profiles/_profile.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! profile, :id, :description, :skills, :linkedin_url, :age, :city, :user_id, :sector_id, :created_at, :updated_at
-json.url profile_url(profile, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :profiles
+  resources :profiles, only: [:show, :update, :edit]
   root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
