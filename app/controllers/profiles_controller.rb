cce94db0f90b2a545e6b64d23ae2c77488e18aa8
Halima-C <h.cherik@gmail.com>
2020-09-08 21:31:25 +0200

    add relation to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,32 +1,42 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update]
-
+  before_action :authenticate_user!
 
 
   def show
   end
 
+  def new
+    @profile = Profile.new
+  end
 
 
   def edit
   end
 
 
-  
 
   def update
     respond_to do |format|
       if @profile.update(profile_params)
-        format.html { redirect_to @profile, notice: 'Vous venez de modifier votre profil avec succès.' }
-  
+        format.html { redirect_to @profile, notice: 'Vous venez de modifier votre profil avec succès' }
+    
       else
         format.html { render :edit }
- 
+        
       end
     end
   end
 
-
+  # DELETE /profiles/1
+  # DELETE /profiles/1.json
+  def destroy
+    @profile.destroy
+    respond_to do |format|
+      format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :profiles, only: [:show, :update, :edit]
+  resources :profiles, only: [:show, :update, :edit], path: :profil
   root to: 'pages#home'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
