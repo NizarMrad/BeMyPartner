5774dc655d27079ccb859b30470df87eaadc622a
Halima-C <h.cherik@gmail.com>
2020-09-08 16:59:29 +0200

    add profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_094432) do
+ActiveRecord::Schema.define(version: 2020_09_08_141733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.text "description"
+    t.string "skills"
+    t.string "linkedin_url"
+    t.integer "age"
+    t.string "city"
+    t.integer "user_id"
+    t.integer "sector_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
