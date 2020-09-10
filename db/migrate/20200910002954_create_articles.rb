0a5c517378c9bf38d781f135140395d720acca31
name <aleatoir3@gmail.com>
2020-09-10 23:31:50 +0200

    user authentified can change article

--- db/migrate/20200910002954_create_articles.rb
+++ db/migrate/20200910002954_create_articles.rb
@@ -2,7 +2,10 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
       t.string :title
+      t.integer :sector_id
       t.text :content
+      t.belongs_to :user, foreign_key: true
+
 
       t.timestamps
     end
