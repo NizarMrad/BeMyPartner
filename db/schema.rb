b8b86d1cc0ca2631783abd737deee1165d87e227
NizarMrad <nizar.mrad.pro@gmail.com>
2020-09-11 13:43:15 +0200

    front test and new migration

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2020_09_10_002954) do
     t.string "title"
     t.integer "sector_id"
     t.text "content"
-
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -57,10 +56,10 @@ ActiveRecord::Schema.define(version: 2020_09_10_002954) do
     t.string "linkedin_url"
     t.integer "age"
     t.string "city"
-    t.bigint "user_id"
     t.integer "sector_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
@@ -86,7 +85,6 @@ ActiveRecord::Schema.define(version: 2020_09_10_002954) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
