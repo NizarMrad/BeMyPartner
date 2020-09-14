22f7f94b0a5f6b32b7647c25298ddabd065b0300
M'rad <46321950+NizarMrad@users.noreply.github.com>
2020-09-14 13:11:20 +0200

    Update connexion cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,16 +4,17 @@ module ApplicationCable
 
     def connect
       self.current_user = find_verified_user
+      logger.add_tags 'ActionCable', current_user.name
     end
 
     protected
-
-    def find_verified_user
-      if (current_user = env['warden'].user)
-        current_user
-      else
-        reject_unauthorized_connection
+      def find_verified_user
+        verified_user = User.find_by(id: cookies.signed['user.id'])
+        if verified_user && cookies.signed['user.expires_at'] > Time.now
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
       end
-    end
   end
 end
