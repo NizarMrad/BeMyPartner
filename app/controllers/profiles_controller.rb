273f1b5ed5756895ce2a46ff500c767ba754a5bb
NizarMrad <nizar.mrad.pro@gmail.com>
2020-09-15 13:46:29 +0200

    s3 setup fixed

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -69,6 +69,6 @@ class ProfilesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def profile_params
-      params.require(:profile).permit(:name, :description,:linkedin_url, :skills, :sector_id,:city, :age, :user_id, :profile_picture)
+      params.require(:profile).permit(:name, :description,:linkedin_url, :skills, :sector_id,:city, :age, :user_id, :avatar)
     end
 end
