4ac8c496f517af877ca882ca8c483d2f8079cca8
LeDjinn <timour.spiridonov@gmail.com>
2020-09-15 12:55:23 +0200

    Fixed $ bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,15 +39,13 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.url = 'wss://bemypartner-staging.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://bemypartner-staging.herokuapp.com/', 'http://bemypartner-staging.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://bemypartner-staging.herokuapp.com, http://bemypartner-staging.herokuapp.com ']
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -66,9 +64,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "BeMyPartner_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {host: "https://bemypartner-staging.herokuapp.com/"}
-  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
