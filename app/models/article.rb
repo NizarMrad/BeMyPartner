3d34911c60d2b405b6ee41f8050af5f35e0477d3
Halima-C <h.cherik@gmail.com>
2020-09-15 17:31:38 +0200

    validates for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
 	belongs_to :user
 
+	validates :title, presence: true , length: { in: 5..140, message: "A title must be at least 5 characters long" }
+	validates :content, presence: true , length: {in: 20..10000, message: "Content's size must be at least 20 characters long and must not exceed 10000 characters" }
 end
 
