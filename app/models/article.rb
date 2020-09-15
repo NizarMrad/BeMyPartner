681bbf2139263b2810ab8ef24470b0ca0bb2deed
Halima-C <h.cherik@gmail.com>
2020-09-15 18:00:09 +0200

    correct validate and button article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
 	belongs_to :user
 
-	validates :title, presence: true , length: { in: 5..140, message: "A title must be at least 5 characters long" }
-	validates :content, presence: true , length: {in: 20..10000, message: "Content's size must be at least 20 characters long and must not exceed 10000 characters" }
+	validates :title, presence: true , length: { in: 5..140, message: "must be at least 5 characters long" }
+	validates :content, presence: true , length: {in: 20..10000, message: "must be at least 20 characters long and must not exceed 10000 characters" }
+   
 end
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_one :sector
+
+  validates :title, presence: true , length: { in: 5..140, message: "A title must be at least 5 characters long" }
+  validates :description, presence: true , length: {in: 100..5000, message: "Description's size must be at least 100 characters long and must not exceed 5000 characters" }
+
+
 end
