08621e3a453790528d2de7ad62f501b26b6b076d
name <aleatoir3@gmail.com>
2020-09-17 15:27:27 +0200

    remove migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_092436) do
+ActiveRecord::Schema.define(version: 2020_09_14_150800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,8 @@ ActiveRecord::Schema.define(version: 2020_09_16_092436) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.integer "sector_id"
     t.text "content"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
@@ -101,27 +100,28 @@ ActiveRecord::Schema.define(version: 2020_09_16_092436) do
   end
 
   create_table "profiles", force: :cascade do |t|
+    t.string "name"
     t.text "description"
     t.string "skills"
     t.string "linkedin_url"
     t.integer "age"
     t.string "city"
+    t.bigint "user_id", null: false
     t.integer "sector_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.string "name"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "budget_id"
-    t.string "profile_needed"
+    t.string "description"
     t.string "files"
+    t.text "title"
+    t.string "sector"
+    t.integer "budget"
     t.string "city"
-    t.bigint "user_id"
+    t.string "profile_needed"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_092436) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
