6f0236b54941d7a279b16ab91825448194cb46b0
Halima-C <h.cherik@gmail.com>
2020-09-18 17:08:03 +0200

    controller for order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     def destroy
       @user = User.find(params[:id])
       @user.destroy
-      redirect_to users_path, notice: 'User deleted.'
-    end
+     
+     end
+
   end
